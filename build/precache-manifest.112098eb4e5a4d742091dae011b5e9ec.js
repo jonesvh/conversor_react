40a33f114333abbba4d11de525ed467547fce330
jones <jonesveriato@gmail.com>
2020-03-26 22:20:16 -0300

    heroku

---
+++ build/precache-manifest.112098eb4e5a4d742091dae011b5e9ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649fc2fe26c066f6ab8aa22bb1f40a7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "275090792ecded6a0189",
+    "url": "/static/css/main.352bae0a.chunk.css"
+  },
+  {
+    "revision": "b2efc5a3724fd4875fd2",
+    "url": "/static/js/2.4c1c825d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c1c825d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "275090792ecded6a0189",
+    "url": "/static/js/main.2f382d81.chunk.js"
+  },
+  {
+    "revision": "4d3918f8d5d21693f110",
+    "url": "/static/js/runtime-main.d4324ca5.js"
+  },
+  {
+    "revision": "1a3988a2b0baec570793fdd76cc66c48",
+    "url": "/static/media/euaFlag.1a3988a2.png"
+  },
+  {
+    "revision": "bb8ed3c4bddb6716e97e3da700c2eff1",
+    "url": "/static/media/euroFlag.bb8ed3c4.jpg"
+  }
+]);
