37a9f80f83f4bb400ef677fb099227be83ce00ff
jones <jonesveriato@gmail.com>
2020-04-01 03:12:12 -0300

    test

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 3000;
-const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
+const express = require('express')
+const favicon = require('express-favicon')
+const path = require('path')
+const port = process.env.PORT || 3000
+const app = express()
+app.use(favicon(__dirname + '/build/favicon.ico'))
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'build')))
 app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+  return res.send('pong')
+})
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port, ()=>{console.log('listen port 3000')});
+  if (req.headers['x-forwarded-proto'] != 'https') {
+    res.redirect('https://' + req.headers.host + req.url)
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  }
+})
+app.listen(port, () => {
+  console.log('listen port 3000')
+})
